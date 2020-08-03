02472f8dffc62125beed6aba3763d559ac6e2f78
Gaurav <itsgaurav01@gmail.com>
2020-08-03 14:35:22 +0530

    Minor changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,8 +14,8 @@ export default class About extends Component {
                     <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
                     <p id="about-text1">I am an undergraduate student at National Institute of Technology Warangal pursuing B.Tech. in Computer Science Engineering. I am interested in exploring new technologies and improving my skills to build applications which solve real-life problems. </p>
-                    <p id="about-text2">I enjoy solving algorithmic problems and am an active competitive programmer on various platforms like <a href="https://codeforces.com/profile/gaurav_512">Codeforces</a>, <a href="https://www.codechef.com/users/ahambrahmasmi">Codechef</a>, <a href="https://hackerrank.com/gaurav512">HackerRank</a>, etc. 
-                    I have a strong academic background. Currently looking to build beautiful, powerful things and share them with the world.</p>
+                    <p id="about-text2">I have a strong academic background. I enjoy solving algorithmic problems and am an active competitive programmer on various platforms like <a href="https://codeforces.com/profile/gaurav_512" target="_blank" rel="noopener noreferrer">Codeforces</a>, <a href="https://www.codechef.com/users/ahambrahmasmi" target="_blank" rel="noopener noreferrer">Codechef</a>, <a href="https://hackerrank.com/gaurav512" target="_blank" rel="noopener noreferrer">HackerRank</a>, etc. 
+                    </p>
 
                     </div>
                 </div>
